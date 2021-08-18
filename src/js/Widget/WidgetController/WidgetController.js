6812c6cbfc9332c79281af65ef007ed45e5686c2
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-08-18 21:43:25 +0300

    connect to heroku

--- src/js/Widget/WidgetController/WidgetController.js
+++ src/js/Widget/WidgetController/WidgetController.js
@@ -14,14 +14,14 @@ export default class WidgetController {
 
   getNewPosts() {
     const stream$ = new Observable((observer) => {
-      fetch('http://192.168.1.148:7070/posts/latest')
+      fetch('https://yushkevich-posts-server.herokuapp.com/posts/latest')
         .then((response) => response.json())
         .then((result) => {
           result.data.forEach((item) => observer.next(item));
         // observer.complete();
         }).catch((err) => observer.err(err));
     }).pipe(
-      mergeMap((value) => ajax.getJSON(`http://192.168.1.148:7070/posts/${value.id}/comments/latest`).pipe(
+      mergeMap((value) => ajax.getJSON(`https://yushkevich-posts-server.herokuapp.com/posts/${value.id}/comments/latest`).pipe(
         map((comments) => {
           value.comments = comments.data;
           return value;
